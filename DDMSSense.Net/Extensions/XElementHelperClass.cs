59aae23b646efac5c25506c99aa2ea3d81842898
Cody Beckner <cody.beckner@gmail.com>
2013-12-11 19:57:22 +0000

    Added XElementHelper

---
+++ DDMSSense.Net/Extensions/XElementHelperClass.cs
@@ -0,0 +1,22 @@
+﻿#region usings
+
+
+
+#endregion
+
+namespace DDMSSense.Extensions
+{
+    #region usings
+
+    using System.Xml.Linq;
+
+    #endregion
+
+    public static class XElementHelperClass
+    {
+        public static string GetPrefix(this XElement element)
+        {
+            return element.GetPrefixOfNamespace(element.Name.Namespace);
+        }
+    }
+}
