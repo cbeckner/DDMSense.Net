1cfebed7e8720763b045a423f38ad977d8096ba4
a-mcalpine <m.aaron.mcalpine@gmail.com>
2014-05-08 15:46:53 -0400

    Added console

---
+++ DDMSenseConsole/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DDMSSense;
+using DDMSSense.Util;
+using DDMSSense.DDMS;
+using System.Xml;
+using System.IO;
+
+namespace DDMSenseConsole
+{
+    class Program
+    {
+        private static Resource _resource;
+
+        static void Main(string[] args)
+        {
+
+            loadFile(new FileInfo(@"D:\Users\aaron.mcalpine.USA\Desktop\ddmsence-src-2.2.0\ddmsence-src-2.2.0\data\sample\4.1-ddmsenceExample.xml"));
+
+             
+
+        }
+
+        private static void loadFile(FileInfo file)
+        {
+            DDMSVersion version = DDMSVersion.GetVersionFor("4.1"); // DDMSVersion.GetCurrentVersion();
+
+            var reader = new DDMSReader();
+
+            _resource = reader.GetDDMSResourceFromFile(file.FullName);
+       
+            string xmlFormat = getResource().ToXML();
+            string htmlFormat = getResource().ToHTML();
+            string textFormat = getResource().ToText();
+
+
+
+        }
+
+        /**
+        * Accessor for the currently loaded ddms:resource
+        */
+        private static Resource getResource()
+        {
+            return (_resource);
+        }
+
+    }
+}
