d94668c2fafbacaf592e72da03fc634637f3895c
millerbill3 <millerbill3@gmail.com>
2014-05-19 14:13:17 -0400

    Fixing bugs in BaseComponent

--- DDMSense.Net.Test/BaseComponentTest.cs
+++ DDMSense.Net.Test/BaseComponentTest.cs
@@ -55,11 +55,12 @@ namespace DDMSense.Test
         public virtual void TestBuildIndexInvalidInputBounds()
         {
             Rights rights = new Rights(true, true, true);
-
+            PrivateObject po = new PrivateObject(rights, new PrivateType(typeof(Rights)));
             // Bad total
             try
             {
-                rights.BuildIndex(0, 0);
+                po.Invoke("BuildIndex", new object[] {0, 0} );
+                //rights.BuildIndex(0, 0);
             }
             catch (System.ArgumentException e)
             {
@@ -69,7 +70,8 @@ namespace DDMSense.Test
             // Low index
             try
             {
-                rights.BuildIndex(-1, 1);
+                po.Invoke("BuildIndex", new object[] { -1, 1 });
+                //rights.BuildIndex(-1, 1);
             }
             catch (System.ArgumentException e)
             {
@@ -79,7 +81,8 @@ namespace DDMSense.Test
             // High index
             try
             {
-                rights.BuildIndex(2, 2);
+                po.Invoke("BuildIndex", new object[] { 2, 2 });
+                //rights.BuildIndex(2, 2);
             }
             catch (System.ArgumentException e)
             {
@@ -87,108 +90,126 @@ namespace DDMSense.Test
             }
         }
 
-        //JAVA TO C# CONVERTER WARNING: Method 'throws' clauses are not available in .NET:
-        //ORIGINAL LINE: public void testBuildIndexValidInputBounds() throws DDMSense.Net.Test.DDMS.InvalidDDMSException
+        [TestMethod()]
         public virtual void TestBuildIndexValidInputBounds()
         {
             Rights rights = new Rights(true, true, true);
-
+            PrivateObject po = new PrivateObject(rights, new PrivateType(typeof(Rights)));
             // Good total
-            rights.BuildIndex(0, 1);
+            po.Invoke("BuildIndex", new object[] { 0, 1 });
+            //rights.BuildIndex(0, 1);
 
             // Good index
-            rights.BuildIndex(1, 2);
+            po.Invoke("BuildIndex", new object[] { 1, 2 });
+            //rights.BuildIndex(1, 2);
         }
 
-        //JAVA TO C# CONVERTER WARNING: Method 'throws' clauses are not available in .NET:
-        //ORIGINAL LINE: public void testBuildIndexLevel0() throws DDMSense.Net.Test.DDMS.InvalidDDMSException
+        [TestMethod()]
         public virtual void TestBuildIndexLevel0()
         {
+            string index = string.Empty;
             Rights rights = new Rights(true, true, true);
+            PrivateObject po = new PrivateObject(rights, new PrivateType(typeof(Rights)));
 
             PropertyReader.SetProperty("output.indexLevel", "0");
-            string index = rights.BuildIndex(0, 1);
+
+            index = (string)po.Invoke("BuildIndex", new object[] { 0, 1 });
+            //string index = rights.BuildIndex(0, 1);
             Assert.Equals("", index);
-            index = rights.BuildIndex(2, 4);
+
+            index = (string)po.Invoke("BuildIndex", new object[] { 2, 4 });
+            //index = rights.BuildIndex(2, 4);
             Assert.Equals("", index);
 
             PropertyReader.SetProperty("output.indexLevel", "unknown");
-            index = rights.BuildIndex(0, 1);
+            index = (string)po.Invoke("BuildIndex", new object[] { 0, 1 });
+            //index = rights.BuildIndex(0, 1);
             Assert.Equals("", index);
-            index = rights.BuildIndex(2, 4);
+
+            index = (string)po.Invoke("BuildIndex", new object[] { 2, 4 });
+            //index = rights.BuildIndex(2, 4);
             Assert.Equals("", index);
         }
 
-        //JAVA TO C# CONVERTER WARNING: Method 'throws' clauses are not available in .NET:
-        //ORIGINAL LINE: public void testBuildIndexLevel1() throws DDMSense.Net.Test.DDMS.InvalidDDMSException
+        [TestMethod()]
         public virtual void TestBuildIndexLevel1()
         {
+            string index = string.Empty;
             Rights rights = new Rights(true, true, true);
+            PrivateObject po = new PrivateObject(rights, new PrivateType(typeof(Rights)));
 
             PropertyReader.SetProperty("output.indexLevel", "1");
-            string index = rights.BuildIndex(0, 1);
+            index = (string)po.Invoke("BuildIndex", new object[] { 0, 1 });
+            //string index = rights.BuildIndex(0, 1);
             Assert.Equals("", index);
-            index = rights.BuildIndex(2, 4);
+
+            index = (string)po.Invoke("BuildIndex", new object[] { 2, 4 });
+            //index = rights.BuildIndex(2, 4);
             Assert.Equals("[3]", index);
         }
 
-        //JAVA TO C# CONVERTER WARNING: Method 'throws' clauses are not available in .NET:
-        //ORIGINAL LINE: public void testBuildIndexLevel2() throws DDMSense.Net.Test.DDMS.InvalidDDMSException
+        [TestMethod()]
         public virtual void TestBuildIndexLevel2()
         {
+            string index = string.Empty;
             Rights rights = new Rights(true, true, true);
+            PrivateObject po = new PrivateObject(rights, new PrivateType(typeof(Rights)));
 
             PropertyReader.SetProperty("output.indexLevel", "2");
-            string index = rights.BuildIndex(0, 1);
+            //index = rights.BuildIndex(0, 1);
+            index = (string)po.Invoke("BuildIndex", new object[] { 0, 1 });
             Assert.Equals("[1]", index);
-            index = rights.BuildIndex(2, 4);
+
+
+            //index = rights.BuildIndex(2, 4);
+            index = (string)po.Invoke("BuildIndex", new object[] { 2, 4 });
             Assert.Equals("[3]", index);
         }
 
-        //JAVA TO C# CONVERTER WARNING: Method 'throws' clauses are not available in .NET:
-        //ORIGINAL LINE: public void testBuildOutput() throws DDMSense.Net.Test.DDMS.InvalidDDMSException
+        [TestMethod()]
         public virtual void TestBuildOutput()
         {
             Rights rights = new Rights(true, true, true);
+            PrivateObject po = new PrivateObject(rights, new PrivateType(typeof(Rights)));
             List<IDDMSComponent> objectList = new List<IDDMSComponent>();
             objectList.Add(rights);
-            Assert.Equals("rights.privacyAct: true\nrights.intellectualProperty: true\nrights.copyright: true\n", rights.BuildOutput(false, "", objectList));
+            
+            Assert.Equals("rights.privacyAct: true\nrights.intellectualProperty: true\nrights.copyright: true\n", (string)po.Invoke("BuildOutput", new object[] { false, string.Empty, objectList }));
+                //rights.BuildOutput(false, "", objectList));
 
             List<string> stringList = new List<string>();
             stringList.Add("Text");
-            Assert.Equals("name: Text\n", rights.BuildOutput(false, "name", stringList));
+            Assert.Equals("name: Text\n", (string)po.Invoke("BuildOutput", new object[] { false, "name", stringList }));
+                //rights.BuildOutput(false, "name", stringList));
 
             List<double?> otherList = new List<double?>();
             otherList.Add(Convert.ToDouble(2.0));
-            Assert.Equals("name: 2.0\n", rights.BuildOutput(false, "name", otherList));
+            Assert.Equals("name: 2.0\n", (string)po.Invoke("BuildOutput", new object[] { false, "name", otherList }));
+                //rights.BuildOutput(false, "name", otherList));
         }
 
-        //JAVA TO C# CONVERTER WARNING: Method 'throws' clauses are not available in .NET:
-        //ORIGINAL LINE: public void testSelfEquality() throws DDMSense.Net.Test.DDMS.InvalidDDMSException
+        [TestMethod()]
         public virtual void TestSelfEquality()
         {
             Rights rights = new Rights(true, true, true);
             Assert.Equals(rights, rights);
         }
 
-        //JAVA TO C# CONVERTER WARNING: Method 'throws' clauses are not available in .NET:
-        //ORIGINAL LINE: public void testToString() throws DDMSense.Net.Test.DDMS.InvalidDDMSException
+        [TestMethod()]
         public virtual void TestToString()
         {
             Rights rights = new Rights(true, true, true);
             Assert.Equals(rights.ToString(), rights.ToXML());
         }
 
-        //JAVA TO C# CONVERTER WARNING: Method 'throws' clauses are not available in .NET:
-        //ORIGINAL LINE: public void testVersion() throws DDMSense.Net.Test.DDMS.InvalidDDMSException
+        [TestMethod()]
         public virtual void TestVersion()
         {
             Rights rights = new Rights(true, true, true);
             Assert.Equals(DDMSVersion.CurrentVersion.Namespace, rights.Namespace);
         }
 
-        //JAVA TO C# CONVERTER WARNING: Method 'throws' clauses are not available in .NET:
-        //ORIGINAL LINE: public void testCustomPrefix() throws DDMSense.Net.Test.DDMS.InvalidDDMSException
+        [TestMethod()]
         public virtual void TestCustomPrefix()
         {
             string @namespace = DDMSVersion.CurrentVersion.Namespace;
@@ -198,8 +219,7 @@ namespace DDMSense.Test
             new Language(element);
         }
 
-        //JAVA TO C# CONVERTER WARNING: Method 'throws' clauses are not available in .NET:
-        //ORIGINAL LINE: public void testNullChecks() throws DDMSense.Net.Test.DDMS.InvalidDDMSException
+        [TestMethod()]
         public virtual void TestNullChecks()
         {
             AbstractBaseComponent component = new AbstractBaseComponentAnonymousInnerClassHelper(this);
@@ -224,13 +244,15 @@ namespace DDMSense.Test
             }
         }
 
-        [TestMethod]
+        [TestMethod()]
         public virtual void TestAttributeWarnings()
         {
             AbstractBaseComponent component = new AbstractBaseComponentAnonymousInnerClassHelper2(this);
+            PrivateObject po = new PrivateObject(component, new PrivateType(typeof(AbstractBaseComponent)));
             List<ValidationMessage> warnings = new List<ValidationMessage>();
             warnings.Add(ValidationMessage.NewWarning("test", "locator"));
-            component.AddWarnings(warnings, true);
+            po.Invoke("AddWarnings",new object[] { warnings, true });
+            //component.AddWarnings(warnings, true);
             Assert.Equals("//locator", component.ValidationWarnings[0].Locator);
         }
 
@@ -257,7 +279,7 @@ namespace DDMSense.Test
             }
         }
 
-        [TestMethod]
+        [TestMethod()]
         public virtual void TestSameVersion()
         {
             DDMSVersion.SetCurrentVersion("3.0");
