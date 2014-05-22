2c7e877bc23498d911b711efb8e84addae51d5d7
a-mcalpine <m.aaron.mcalpine@gmail.com>
2014-05-22 14:29:48 -0400

    GetValueOrEmpty ret.toNonNullString()

--- DDMSSense.Net/Extensions/HashMapGetHelperClass.cs
+++ DDMSSense.Net/Extensions/HashMapGetHelperClass.cs
@@ -19,7 +19,7 @@ namespace DDMSense.Extensions
         {
             string ret = string.Empty;
             dictionary.TryGetValue(key, out ret);
-            return Util.Util.GetNonNullString(ret);
+            return ret.ToNonNullString();
         }
     }
 }
